eef5c5831c3b366a0e740ad797f1a98ae5e14b37
Frank Reyes <freyesm91@gmail.com>
2023-03-05 10:53:29 -0600

    Add index file

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+require_once 'includes/required-classes.php';
+$main = new Main(); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Title</title>
+</head>
+<body>
+<?php
+    $main->runCases();
+ ?>
+</body>
+</html>
